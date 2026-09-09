38a569b85c3c16e798cc4d98efac19d502500b06
Mertkaan <mertkaanmisir@gmail.com>
2026-09-09 15:44:26 +0200

    temp difference in a week

--- c#/Program.cs
+++ c#/Program.cs
@@ -49,3 +49,22 @@ long remaining_days = days % 365;
 long month = remaining_days / 30;
 Console.WriteLine(year + "/" + month);
 
+
+//temp difference in a week
+double monday = 21.5;
+double tuesday = 23.7;
+double wednesday = 19.6;
+double thursday = 22.5;
+double friday = 25.3;
+double saturday = 21.7;
+double sunday = 18.9;
+
+Console.WriteLine(tuesday - monday);
+Console.WriteLine(wednesday - tuesday);
+Console.WriteLine(thursday - wednesday);
+Console.WriteLine(friday - thursday);
+Console.WriteLine(saturday - friday);
+Console.WriteLine(sunday - saturday);
+Console.WriteLine(monday - sunday);
+
+
