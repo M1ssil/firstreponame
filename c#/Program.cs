3b847b3182fa073be4dcec75826a2efeacdb4034
Mertkaan <mertkaanmisir@gmail.com>
2026-09-09 15:49:33 +0200

    average lifespan exercise 5.15

--- c#/Program.cs
+++ c#/Program.cs
@@ -68,3 +68,26 @@ Console.WriteLine(sunday - saturday);
 Console.WriteLine(monday - sunday);
 
 
+// exercise 5.15
+int ada_lovelace = 36; // https://en.wikipedia.org/wiki/Ada_Lovelace
+int dennis_ritchie = 70; // https://en.wikipedia.org/wiki/Dennis_Ritchie
+int grace_hopper = 85; // https://en.wikipedia.org/wiki/Grace_Hopper
+int hedy_lamarr = 85; // https://en.wikipedia.org/wiki/Hedy_Lamarr
+int edsger_dijkstra = 72; // https://en.wikipedia.org/wiki/Edsger_W._Dijkstra
+int douglas_engelbart = 88; // https://en.wikipedia.org/wiki/Douglas_Engelbart
+float male_avg = (float)(dennis_ritchie + edsger_dijkstra + douglas_engelbart) / 3;
+float female_avg = (float)(ada_lovelace + grace_hopper + hedy_lamarr) / 3;
+float avg = (male_avg + female_avg) / 2;
+float diff = male_avg - female_avg;
+Console.Write("Average lifespan of a male computer scientist: ");
+Console.WriteLine(male_avg);
+Console.Write("Average lifespan of a female computer scientist: ");
+Console.WriteLine(female_avg);
+Console.Write("Average lifespan of a computer scientist: ");
+Console.WriteLine(avg);
+Console.Write("Males live this much longer than females: ");
+Console.WriteLine(diff);
+/* in this coding we see that we first start with giving all variables a number and afterwards finding
+* the average lifespan of each gender and then we take both answers and see the difference between the genders 
+*/
+
