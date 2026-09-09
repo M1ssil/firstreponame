48049ec71a55e5ac9d4f69494bbed952e75ed170
Mertkaan <mertkaanmisir@gmail.com>
2026-09-09 14:42:41 +0200

    area of a circle special edition

--- c#/Program.cs
+++ c#/Program.cs
@@ -18,5 +18,13 @@ double d = 2.0;
 int i = (int) d;
 // når man skal få det til at blive et helt tal
 
-bool b = true;
-// true or false
+//bool true eller false
+
+// beregner arealet af en cirkel
+double Area(double radius){
+    return Math.PI * radius * radius;
+}
+Console.WriteLine(Area(1));
+Console.WriteLine(Area(3));
+Console.WriteLine(Area(5));
+
