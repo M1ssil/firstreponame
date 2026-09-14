97339e51a4621e3097a8a9929ba79aaabd55092a
Mertkaan <mertkaanmisir@gmail.com>
2026-09-14 10:56:12 +0200

    else if statements

--- c#/Program.cs
+++ c#/Program.cs
@@ -143,4 +143,19 @@ Console.WriteLine("B");
 }
 */
 
+/* else if statements (change char direction to get a certain line)
+char direction = 'n';
+if (direction=='N') {
+Console.WriteLine("Going north ...");
+} else if (direction=='S') {
+Console.WriteLine("Going south ...");
+} else if (direction=='E') {
+Console.WriteLine("Going east ...");
+} else if (direction=='W') {
+Console.WriteLine("Going west ...");
+} else {
+Console.WriteLine("I don't understand ?!?");
+}
+*/
+
 
