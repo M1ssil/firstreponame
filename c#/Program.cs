8176d4a56459b13be354d13af636d1770107706e
Mertkaan <mertkaanmisir@gmail.com>
2026-09-16 13:07:48 +0200

    reference manipulation

--- c#/Program.cs
+++ c#/Program.cs
@@ -409,6 +409,24 @@ average = total / doubleArray.Length;
 */
 
 
+//Reference Manipulation
+/* Leapyear
+int[] monthsNormal = [31,28,31,30,31,30,31,31,30,31,30,31];
+int[] monthsLeap = [31,29,31,30,31,30,31,31,30,31,30,31];
+for (int i=0 ; i<2020 ; i++) {
+either do this -> : int[] months = monthsNormal;
+                    if (i%4==0){
+                    months = monthsLeap;
+or do this -> : int[] months = (i%4==0 ? monthsLeap : monthsNormal);
+}
+Console.WriteLine("In year "+i+" February is "+months[1]+
+" days long");
+}
+*/
+
+
+// Arrays of Arrays
+
 
 
 // Øvelsestime 16/09
